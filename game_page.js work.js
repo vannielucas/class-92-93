3e1640bc2908ad911a88aeef8216f2f7ff6388e2
Ethan <81496693+vannielucas@users.noreply.github.com>
2021-08-03 19:58:40 +1000

    Add files via upload

---
+++ game_page.js work.js
@@ -0,0 +1,10 @@
+//known
+//v+a+r=var
+//github
+player1_name=localStorage.getItem("player1_name")
+player2_name=localStorage.getItem("player2_name") 
+//underscores core
+player1_score=0;
+player2_score=0;
+document.getElementByid("player1_name").innerHTML=player1_name+":";
+document.getElementByid("player2_name").innerHTML=player2_name+":";
